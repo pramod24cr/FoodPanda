deb006ab19759e5f1a45ade11d235c82eb8dcb78
pramod24cr <pramod24cr@gmail.com>
2025-01-17 22:08:20 +0530

    Episode 7

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,6 +1,7 @@
 import RestaurantCard from "./RestaurantCard";
 import { useState, useEffect } from "react";
 import Shimmer from "./Shimmer";
+import { Link } from "react-router";
 
 const Body = () => {
   // Local State Variable - Super powerful variable
@@ -71,7 +72,13 @@ const Body = () => {
       </div>
       <div className="res-container">
         {filteredRestaurant.map((restaurant) => (
-          <RestaurantCard key={restaurant.info.id} resData={restaurant} />
+                    <Link
+                    key={restaurant.info.id}
+                    to={"/restaurants/" + restaurant.info.id}
+                  >
+                    <RestaurantCard key={restaurant.info.id} resData={restaurant} />
+                  </Link>
+          // <RestaurantCard key={restaurant.info.id} resData={restaurant} />
         ))}
       </div>
     </div>
---
+++ src/components/RestaurantMenu.js
@@ -0,0 +1,56 @@
+import { useState, useEffect } from "react";
+import Shimmer from "./Shimmer";
+import { useParams } from "react-router";
+import { MENU_API } from "../utils/constants";
+
+const RestaurantMenu = () => {
+  const [resInfo, setResInfo] = useState(null);
+
+  const { resId } = useParams();
+
+  useEffect(() => {
+    fetchMenu();
+  }, []);
+
+  const fetchMenu = async () => {
+    const data = await fetch(MENU_API + resId);
+    const json = await data.json();
+    console.log(json);
+    setResInfo(json.data);
+  };
+
+  if (resInfo === null) return <Shimmer />;
+
+  const { name, cuisines, costForTwoMessage } =
+    resInfo?.cards[2]?.card?.card?.info;
+
+  const itemCards =
+    resInfo?.cards?.[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards?.[1]?.card?.card?.itemCards || [];
+
+
+  console.log(itemCards);
+  return (
+    <div className="menu">
+      <h1>{name}</h1>
+      <p>
+        {cuisines?.join(", ")} - {costForTwoMessage}
+      </p>
+      <h2>Menu</h2>
+      {itemCards.length > 0 ? (
+        <ul>
+          {itemCards.map((item) => (
+            <li key={item.card.info.id}>
+              {item.card.info.name} -{" Rs."}
+              {item.card.info.price / 100 || item.card.info.defaultPrice / 100}
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p>No items available in the menu.</p>
+      )}
+    </div>
+  );
+  
+};
+
+export default RestaurantMenu;
